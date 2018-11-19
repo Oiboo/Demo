393b14efd30436f694c3c1b8ec68694ddd8bc5f9
Oiboo <43292025+Oiboo@users.noreply.github.com>
2018-11-19 17:08:24 +0000

    First Commit

---
+++ Phaser_Template/main.js
@@ -0,0 +1,7 @@
+
+
+/*  load game canvase size (w,h,render-er(canvas))   */
+var game = new Phaser.Game(600,400,Phaser.AUTO);
+/*Add gamestate */
+game.state.add('state1', demo.state1);
+game.state.start('state1');

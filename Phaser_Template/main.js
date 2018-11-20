e10e871ce8179ef92423107acd6b85d6597d231c
Oiboo <43292025+Oiboo@users.noreply.github.com>
2018-11-20 15:39:49 +0000

    Delete main.js

--- Phaser_Template/main.js
+++
@@ -1,8 +0,0 @@
-
-
-/*  load game canvase size (w,h,render-er(canvas))   */
-var game = new Phaser.Game(1500,1000,Phaser.AUTO);
-/*Add gamestate */
-game.state.add('state1', demo.state1);
-game.state.add('state2', demo.state2);
-game.state.start('state1');
